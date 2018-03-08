f79e42a811ca718838263e4248b3294de2e48a65
Chirag Jain <chiragjain.udr27@gmail.com>
2018-03-09 00:13:04 +0530

    Add files via upload

--- univariate_linear_regression.py
+++ univariate_linear_regression.py
@@ -4,4 +4,33 @@ Created on Thu Mar  8 23:44:58 2018
 
 @author: chirag
 """
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+dataset=pd.read_csv('Salary_Data.csv')
+X=dataset.iloc[:,:-1]
+y=dataset.iloc[:,1]
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=1/3,random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred=regressor.predict(X_test)
+
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title='Salary vs Experience'
+plt.xlabel='Experience'
+plt.ylabel='Salary'
+plt.show()
+
+plt.scatter(X_test,y_test,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title='Salary vs Experience'
+plt.xlabel='Experience'
+plt.ylabel='Salary'
+plt.show()
